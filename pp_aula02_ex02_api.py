aaaa406127c41b220aad270eb0192d94b8611faf
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-30 19:15:17 -0300

    Update pp_aula02_ex02_api.py

--- pp_aula02_ex02_api.py
+++ pp_aula02_ex02_api.py
@@ -6,7 +6,7 @@ import requests
 cpf = '48263463874'
 url = f'https://api.cpfhub.io/cpf/{cpf}'
 headers = {
-    'x-api-key': 'ad43d4c69f4844d689de7fb683999a40ca086505b068f5f7bc695e9b39e76fca',
+    'x-api-key': 'SUA_CHAVE_DE_API_AQUI',
     'Accept': 'application/json'
 }
 
